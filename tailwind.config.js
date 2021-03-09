fbbf724519bc4715473f21d2ddab86b09a8daf42
arturmolenda <apmolenda@gmail.com>
2021-03-09 09:53:23 +0100

    TailwindCSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        turquoise: {
+          light: "#83fcff",
+          default: "#58e3e6",
+          dark: "#39b9bc",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
