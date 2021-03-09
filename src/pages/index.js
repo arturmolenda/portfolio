bfeab781afef1410a20f3827ade46ef1ea702867
arturmolenda <apmolenda@gmail.com>
2021-03-09 23:10:52 +0100

    bring new Home screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import * as React from "react"
 
 import Layout from "../components/Layout"
+import Home from "../components/sections/Home"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Artur Molenda" />
-    <h1>Hello World</h1>
+    <Home />
   </Layout>
 )
 
